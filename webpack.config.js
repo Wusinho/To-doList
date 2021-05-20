06ac5c374594f5065a5d7a3b03df2d1008ceae83
Heber <heberlazo@gmail.com>
2021-05-20 11:35:15 -0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const config = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: "file-loader",
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              mimetype: "image/png",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      appMountId: "app",
+      filename: "index.html",
+    }),
+  ],
+};
+
+module.exports = config;
