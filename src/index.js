5154dd8333608a405ad9a0cdc1f79dc845c93720
Heber <heberlazo@gmail.com>
2021-05-28 20:49:29 -0500

    Bug with render

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 /* no-plusplus */
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './main.scss';
-import bodyView from './modules/body';
-import navBar from './modules/navbar';
-import Project from './modules/project';
-import Task from './modules/task';
-import Store from './modules/store';
-import UI from './modules/UI';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./main.scss";
+import bodyView from "./modules/body";
+import navBar from "./modules/navbar";
+import Project from "./modules/project";
+import Task from "./modules/task";
+import Store from "./modules/store";
+import UI from "./modules/UI";
 
-const getRoot = document.getElementById('root');
-const getNavbar = document.getElementById('navbar');
+const getRoot = document.getElementById("root");
+const getNavbar = document.getElementById("navbar");
 
 getNavbar.appendChild(navBar());
 getRoot.appendChild(bodyView());
 
-document.getElementById('job-form').addEventListener('submit', (e) => {
-  const addjob = document.getElementById('job').value;
+document.getElementById("job-form").addEventListener("submit", (e) => {
+  const addjob = document.getElementById("job").value;
 
   if (addjob) {
     const job = new Project(addjob);
@@ -28,28 +28,28 @@ document.getElementById('job-form').addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-document.getElementById('sidebar').addEventListener('click', (e) => {
+document.getElementById("sidebar").addEventListener("click", (e) => {
   const array = Store.getJobs();
 
   for (let i = 0; i < array.length; i += 1) {
     if (e.target.id === array[i].id) {
-      const getFormTasks = document.getElementById('task-form');
-      getFormTasks.style.setProperty('display', 'inline-block');
+      const getFormTasks = document.getElementById("task-form");
+      getFormTasks.style.setProperty("display", "inline-block");
       // const getMenuli = document.getElementById("menu-li");
 
       // getMenuli.style.setProperty("display", "inline-block");
 
-      document.getElementById('task-form').addEventListener('submit', (e) => {
-        const chore = document.getElementById('chore').value;
-        const date = document.getElementById('date').value;
-        const importance = document.getElementById('importance').value;
+      document.getElementById("task-form").addEventListener("submit", (e) => {
+        const chore = document.getElementById("chore").value;
+        const date = document.getElementById("date").value;
+        const importance = document.getElementById("importance").value;
         if (chore && date && importance) {
           const task = new Task(chore, date, importance);
           Store.addTask(task, i);
           UI.addTaskToMenu(task);
-          const getMenuli = document.getElementById('menu-li');
+          const getMenuli = document.getElementById("menu-li");
 
-          getMenuli.style.setProperty('display', 'inline-block');
+          getMenuli.style.setProperty("display", "inline-block");
         }
         e.preventDefault();
       });
@@ -58,8 +58,8 @@ document.getElementById('sidebar').addEventListener('click', (e) => {
   e.preventDefault();
 });
 
-document.addEventListener('DOMContentLoaded', UI.displayJobs);
+// document.addEventListener("DOMContentLoaded", UI.displayJobs);
 
-document.addEventListener('DOMContentLoaded', UI.displayTasks);
+// document.addEventListener("DOMContentLoaded", UI.displayTasks);
 
 // localStorage.clear();
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,4 +1,4 @@
-import Store from './store';
+import Store from "./store";
 
 class UI {
   static displayJobs() {
@@ -13,27 +13,27 @@ class UI {
   }
 
   static addTaskToLibrary(task) {
-    const list = document.getElementById('sidebar');
+    const list = document.getElementById("sidebar");
 
-    const row = document.createElement('li');
-    row.className = 'list-group-item list-group-item-action';
+    const row = document.createElement("li");
+    row.className = "list-group-item list-group-item-action";
     row.innerHTML = `${task.job} `;
     row.id = `${task.id}`;
     list.appendChild(row);
   }
 
   static addTaskToMenu(task) {
-    const list = document.getElementById('menu-details');
+    const list = document.getElementById("menu-details");
 
-    const row = document.createElement('li');
-    row.className = 'list-group-item ';
-    row.id = 'menu-li';
+    const row = document.createElement("li");
+    row.className = "list-group-item ";
+    row.id = "menu-li";
     row.innerHTML = `${task.chore}  ${task.date} ${task.importance} `;
     list.appendChild(row);
   }
 
   static clearField() {
-    document.getElementById('job').value = '';
+    document.getElementById("job").value = "";
   }
 }
 export default UI;
