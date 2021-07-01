2524d0603ebb99151ddccc982468d72b93fa222d
Heber <heberlazo@gmail.com>
2021-06-30 20:48:16 -0500

    Added npm build with linters fix

--- src/index.js
+++ src/index.js
@@ -1,55 +1,46 @@
-/* eslint-disable */
-
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./main.scss";
-import createInputParent from "./modules/inputProject";
-import Task from "./modules/task";
-import Project from "./modules/project";
-import iFinder from "./modules/iFinder";
-import addCounter from "./modules/addCounter";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './main.scss';
+import createInputParent from './modules/inputProject';
+import Project from './modules/project';
 import {
   getInput,
   realDetail,
-  getChildInput,
   afterInput,
-} from "./modules/editMethod";
+} from './modules/editMethod';
 import {
   displayProjects,
   addTasksToProject,
   displayTaks,
   removeEmptyTasks,
-} from "./modules/UI";
-import { setLocal, getLocal, setLocalObject } from "./modules/localStorage";
-const getRoot = document.getElementById("root");
+} from './modules/UI';
+import { setLocal, getLocal, setLocalObject } from './modules/localStorage';
 
 createInputParent();
-const getParentInput = document.getElementsByClassName("parent");
-const getChildrenInput = document.getElementsByClassName("children");
+const getParentInput = document.getElementsByClassName('parent');
+const getChildrenInput = document.getElementsByClassName('children');
 
-for (var i = 0; i < getChildrenInput.length; i++) {
-  getChildrenInput[i].addEventListener("click", (e) => {
+for (let i = 0; i < getChildrenInput.length; i += 1) {
+  getChildrenInput[i].addEventListener('click', (e) => {
     if (!e.target) return;
     const eValue = e.target.id;
     const realName = eValue.slice(0, -1);
     const taskSelect = eValue.slice(-1);
-    setLocal("+realName", realName);
-    setLocal("+detail", realDetail(taskSelect));
+    setLocal('+realName', realName);
+    setLocal('+detail', realDetail(taskSelect));
 
     const getElement = document.getElementById(eValue);
 
     const inputVariable = JSON.parse(getLocal(realName));
 
     inputVariable[realDetail(taskSelect)] = inputVariable;
-    getElement.addEventListener("input", afterInput);
+    getElement.addEventListener('input', afterInput);
 
-
-    removeEmptyTasks()
-
+    removeEmptyTasks();
   });
 }
 
-for (var i = 0; i < getParentInput.length; i++) {
-  getParentInput[i].addEventListener("click", (e) => {
+for (let i = 0; i < getParentInput.length; i += 1) {
+  getParentInput[i].addEventListener('click', (e) => {
     if (!e.target) return;
     const inputId = e.target.id;
 
@@ -59,7 +50,7 @@ for (var i = 0; i < getParentInput.length; i++) {
       const newProject = new Project(inputId);
       setLocalObject(inputId, newProject);
       addTasksToProject(e.target.id);
-    } else if (getElement.value){
+    } else if (getElement.value) {
       addTasksToProject(e.target.id);
     }
 
@@ -67,11 +58,10 @@ for (var i = 0; i < getParentInput.length; i++) {
 
     if (inputVariable) inputVariable.chore = inputVariable;
 
-    getElement.addEventListener("input", getInput);
-
+    getElement.addEventListener('input', getInput);
   });
 }
 
 displayProjects();
 displayTaks();
-removeEmptyTasks()
+removeEmptyTasks();
